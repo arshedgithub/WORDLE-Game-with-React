15aef40ab583e715d626789485ee6d4a407064b0
Arshed Ahmed <arshedahmedmail@gmail.com>
2022-05-15 22:32:55 +0530

    displaying current guess

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Row({ guess }) {
+export default function Row({ guess, currentGuess }) {
   // if guessed word is available
   if (guess) {
     return (
@@ -9,6 +9,23 @@ export default function Row({ guess }) {
           <div key={i} className={l.color}>
             {l.key}
           </div>
+        ))}
+      </div>
+    );
+  }
+
+  if (currentGuess) {
+    let letters = currentGuess.split("");
+
+    return (
+      <div className="row current">
+        {letters.map((letter, i) => (
+          <div key={i} className="filled">
+            {letter}
+          </div>
+        ))}
+        {[...Array(5 - letters.length)].map((_, i) => (
+          <div key={i}></div>
         ))}
       </div>
     );
