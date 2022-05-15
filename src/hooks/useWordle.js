25e784f76c9bf2d03250546f3e0b08d7f66fc416
Arshed Ahmed <arshedahmedmail@gmail.com>
2022-05-15 14:36:17 +0530

    submitting guesses

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -4,12 +4,14 @@ const useWordle = (solution) => {
   const [turn, setTurn] = useState(0);
   const [currentGuess, setCurrentGuess] = useState("");
   const [guesses, setGuesses] = useState([]); // each guess is an array
-  const [history, setHistory] = useState([]); // each guess is a string
+  const [history, setHistory] = useState(["hello"]); // each guess is a string
   const [isCorrect, setIsCorrect] = useState(false);
 
   // format a guess into an array of letter objects
   // e.g. [{key: 'a', color: 'yellow'}]
-  const formatGuess = () => {};
+  const formatGuess = () => {
+    console.log("formating guess", currentGuess);
+  };
 
   // add a new guess to the guesses state
   // update the isCorrect state if the guess is correct
@@ -19,19 +21,42 @@ const useWordle = (solution) => {
   // handle keyup event & track current guess
   // if user presses enter, add the new guess
   const handleKeyup = ({ key }) => {
-    console.log(key);
+    if (key === "Enter") {
+      // only add guessed word if turn is less than 5
+      if (turn > 5) {
+        console.log("You used all of your turns");
+        return;
+      }
+
+      // do not allowed duplicate words
+      if (history.includes(currentGuess)) {
+        console.log("You already tried that word");
+        return;
+      }
+
+      // check word is 5 chars long
+      if (currentGuess.length !== 5) {
+        console.log("Word must be 5 chars");
+        return;
+      }
+
+      formatGuess();
+    }
+
+    // backspacing the guessed word
     if (key === "Backspace") {
       setCurrentGuess((prev) => {
         return prev.slice(0, -1);
       });
       return;
     }
+
+    // check pressed key is a letter
     if (/^[A-Za-z]$/.test(key)) {
       if (currentGuess.length < 5) {
         setCurrentGuess((prev) => {
           return prev + key;
         });
-        console.log(currentGuess);
       }
     }
   };
