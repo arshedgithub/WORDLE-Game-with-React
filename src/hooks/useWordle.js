d7752206bb0dea39b8f8aa0fecd601b9f9658e41
Arshed Ahmed <arshedahmedmail@gmail.com>
2022-05-26 18:35:53 +0530

    highlighted the selected keys

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -6,6 +6,7 @@ const useWordle = (solution) => {
   const [guesses, setGuesses] = useState([...Array(6)]); // each guess is an array
   const [history, setHistory] = useState(["hello"]); // each guess is a string
   const [isCorrect, setIsCorrect] = useState(false);
+  const [usedKeys, setUsedKeys] = useState({});
 
   // format a guess into an array of letter objects
   // e.g. [{key: 'a', color: 'yellow'}]
@@ -51,6 +52,31 @@ const useWordle = (solution) => {
     });
     setTurn((prevTurn) => {
       return prevTurn + 1;
+    });
+
+    setUsedKeys((prevUsedKeys) => {
+      let newKeys = { ...prevUsedKeys };
+
+      formattedGuess.forEach((l) => {
+        const currentColor = newKeys[l.key];
+        if (l.color === "green") {
+          newKeys[l.key] = "green";
+          return;
+        }
+        if (l.color === "yellow" && currentColor !== "green") {
+          newKeys[l.key] = "yellow";
+          return;
+        }
+        if (
+          l.color === "grey" &&
+          currentColor !== "green" &&
+          currentColor !== "green"
+        ) {
+          newKeys[l.key] = "grey";
+          return;
+        }
+      });
+      return newKeys;
     });
     setCurrentGuess("");
   };
@@ -99,7 +125,7 @@ const useWordle = (solution) => {
     }
   };
 
-  return { turn, currentGuess, guesses, isCorrect, handleKeyup };
+  return { turn, currentGuess, guesses, isCorrect, usedKeys, handleKeyup };
 };
 
 export default useWordle;
